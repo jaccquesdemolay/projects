ca9416e78dbd8491169ec70e38d06910578f53f1
jaccquesdemolay <97102422+jaccquesdemolay@users.noreply.github.com>
2023-03-02 21:05:07 +0000

    removing db details

--- dbconnection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost:3308";
-    $database = "eventswebpg";
-    $username = "root";
-    $pwd = "123";
-
-    $con = mysqli_connect($servername, $username, $pwd, $database) or die ("Error connecting to the database");
-
-?>
